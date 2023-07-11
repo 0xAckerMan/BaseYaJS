cc7922d26f7b78a7fecc23d87d30ff9ef40b2b56
0xAckerMan <joelkores1@gmail.com>
2023-07-11 15:50:42 +0300

    Js compiled output

---
+++ TypeScript/warmup/dist/test.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const another_1 = require("./another");
+console.log("hello world");
+const person = new another_1.Person();
+person.cry();
+//# sourceMappingURL=test.js.map
