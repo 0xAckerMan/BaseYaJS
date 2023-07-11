f35910e73471ef4c49ca828923b33be4ee0d0d5a
0xAckerMan <joelkores1@gmail.com>
2023-07-11 15:54:01 +0300

    Hello from Typescript

---
+++ TypeScript/Foundation/hello.js
@@ -0,0 +1,3 @@
+console.log("hello world!");
+var intro = { name: "User", age: 10 };
+console.log(intro.age);
---
+++ TypeScript/Foundation/hello.ts
@@ -0,0 +1,6 @@
+console.log("hello world!")
+
+let intro = {name: "User", age: 10}
+
+console.log(intro.age)
+
