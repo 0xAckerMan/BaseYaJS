9461c4369200062668a5a9279067c70407bd15b8
0xAckerMan <joelkores1@gmail.com>
2023-07-13 21:19:32 +0300

    Loop

---
+++ TypeScript/warmup/src/7-multi_c.ts
@@ -0,0 +1,13 @@
+// Prints "C is fun" according to the number of args
+
+const num = parseInt(process.argv[2]);
+
+if (isNaN(num)){
+    console.log("Missing number of occurrences")
+} else {
+    let i = 0;
+    while (i < num) {
+        console.log("C is fun");
+        i++;
+    }
+}
