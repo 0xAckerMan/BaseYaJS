b48cafe8eb999433aa649f11b5b9c6173ddecafc
0xAckerMan <joelkores1@gmail.com>
2023-07-13 21:19:13 +0300

    While loop

---
+++ TypeScript/warmup/src/6-multi_language_loop.ts
@@ -0,0 +1,10 @@
+// A script that prints 3 lines using an array of stings and a loop
+
+const lines = ["C is fun", "Python is cool", "Typescript is amazing"]
+
+let i = 0;
+
+while(i < lines.length){
+    console.log(lines[i]);
+    i++;
+}
